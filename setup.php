8cdc3e7d3231d285002198f2d2b4f87b09f99b0a
CJ Willcock <cj@cjwillcock.ca>
2011-10-30 00:26:11 -0300

    Fixed formatting on completion message.

--- setup.php
+++ setup.php
@@ -287,7 +287,8 @@
 	if(file_exists('data/config_required'))
 	{
 	    @unlink('data/config_required');
-	    print_message( "Setup is now complete. <a href='/' title='Continue'>Continue</a> to your installation." );
+	    print_header( "Setup is complete." );
+	    print_message( "<a href='/' title='Continue'>Continue</a> to your installation." );
 	}
     }
 
